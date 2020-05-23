c191620ba35c63cd0b667effe87da954900d5741
takimotoRyoichi <cc.c2.npr@gmail.com>
2020-05-23 10:06:11 +0900

    Restoration package.json

---
+++ nounou/plugins/element.js
@@ -0,0 +1,7 @@
+import Vue from './node_modules/vue'
+import Element from './node_modules/element-ui'
+import '../element-variables.scss'
+import locale from './node_modules/element-ui/lib/locale/lang/ja'
+
+// Elementを起動
+Vue.use(Element, { locale })
---
+++ nounou/src/plugins/element.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Element from 'element-ui'
+import '../element-variables.scss'
+import locale from 'element-ui/lib/locale/lang/ja'
+
+// Elementを起動
+Vue.use(Element, { locale })
